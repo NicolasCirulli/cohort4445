57fe3c85bea8c2b694234c2693231c9a9cb7e4c7
NicolasCirulli <nicolascirulli2@gmail.com>
2023-02-28 10:49:23 -0300

    workshop bucles

---
+++ bucles/main.js
@@ -0,0 +1,55 @@
+const mentores = [
+    {
+        nombre: 'Nicolas',
+        edad : 28,
+        apellido: 'Cirulli',
+        rol: 'Mentor/a',
+        especialidad : ['JavaScript', 'React']
+    },
+    {
+        nombre: 'Lucrecia',
+        edad : 27,
+        apellido: 'Gillone',
+        rol: 'Mentor/a',
+        especialidad : ['JavaScript', 'React']
+    },
+    {
+        nombre: 'Florencia',
+        edad : 22,
+        apellido: 'Puñales',
+        rol: 'Mentor/a',
+        especialidad : ['HTML', 'CSS', 'Vue']
+    },
+    {
+        nombre: 'Camila',
+        edad : 28,
+        apellido: 'Domato',
+        rol: 'Mentor/a',
+        especialidad : ['HTML', 'CSS', 'Bootstrap']
+    },
+    {
+        nombre: 'Kevin',
+        edad : 30,
+        apellido: 'Darnet',
+        rol: 'Mentor/a',
+        especialidad : ['HTML', 'JavaScript']
+    },
+    {
+        nombre: 'Guillermo',
+        edad : 26,
+        apellido: 'Bonutto',
+        rol: 'Mentor/a',
+        especialidad : ['Java', 'Vue']
+    },
+];
+const numeros = [1,2,3,4,5,6,7,8,9,10]
+
+
+const mentoresReact = [];
+
+let indiceLucre;
+
+
+
+
+
